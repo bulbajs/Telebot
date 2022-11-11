a17131bc9ba38547829f47788bccdfa3d2f859dd
bulbajs <93475048+bulbajs@users.noreply.github.com>
2022-11-12 00:44:15 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ TOKEN = "5663849580:AAHAiXjSJSXNK3-JFOesdd7RR9DUXcNBIr4"
 keys = {
     'Zcash':'ZEC',
     'Доллар':'USD',
-    'долар': "USD",
     'Евро': 'EUR',
-    'евро': 'EUR',
     'Рубли': 'RUB',
     'Юань': 'CNY'
 
